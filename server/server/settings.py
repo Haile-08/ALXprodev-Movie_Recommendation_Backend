3ce1cb20fd74ef45ef23be70f035a09cd5a5ae90
Haile-08 <haile4cmd@gmail.com>
2025-03-04 14:11:29 +0300

    fix: add cors headers

--- server/server/settings.py
+++ server/server/settings.py
@@ -48,10 +48,12 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     "rest_framework",
     "drf_yasg",
+    'corsheaders',
     "movies",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -168,3 +170,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:5143",
+    "http://localhost:5145",
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
+
+# Allow specific HTTP methods
+CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+
+# Allow specific headers
+CORS_ALLOW_HEADERS = ["content-type", "authorization", "x-requested-with"]

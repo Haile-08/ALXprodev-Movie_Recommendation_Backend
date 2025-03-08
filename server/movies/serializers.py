101ba89206d8fc4571d68e36d3668893ab801445
Haile-08 <haile4cmd@gmail.com>
2025-03-08 09:02:18 +0300

    fix: Exclude user from request data

--- server/movies/serializers.py
+++ server/movies/serializers.py
@@ -12,4 +12,8 @@ class UserSerializer(serializers.ModelSerializer):
 class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Favorite
-        fields = '__all__'
+        exclude = ['user']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        return Favorite.objects.create(user=user, **validated_data)
